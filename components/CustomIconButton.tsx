bfdb4430ee266f87d6b8dd3187930bc769076fb7
Emily Lien <37633033+lienweb@users.noreply.github.com>
2024-04-22 06:44:25 +0800

    fix/custom icon button component

---
+++ components/CustomIconButton.tsx
@@ -0,0 +1,29 @@
+import { Button, Text, Box } from '@chakra-ui/react';
+import Image from 'next/image';
+
+const CustomIconButton = ({ title, display, imgPath, imgBg }) => {
+  return (
+    <Button
+      borderRadius="2xl"
+      variant="outline"
+      color="gray.900"
+      fontWeight="400"
+      display={display}
+      bg="white"
+      _hover={{ color: 'green.100', bg: 'green.50' }}
+    >
+      <Box bg={imgBg} borderRadius="sm">
+        <Image
+          src={imgPath}
+          width={16}
+          height={16}
+          className="hidden md:block"
+        />
+      </Box>
+      <Text fontSize="md" ml={2}>
+        {title}
+      </Text>
+    </Button>
+  );
+};
+export default CustomIconButton;
